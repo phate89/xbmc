137697ef062cc2711306d3b963ab7e032c82facb
phate89 <phates89@gmail.com>
2017-02-02 18:04:48 +0100

    Use our existing python api to set info in music python scrapers (the best we can for now)

--- xbmc/music/tags/MusicInfoTag.h
+++ xbmc/music/tags/MusicInfoTag.h
@@ -51,6 +51,7 @@ public:
   const std::string& GetTitle() const;
   const std::string& GetURL() const;
   const std::vector<std::string>& GetArtist() const;
+  const std::vector<CArtistCredit> GetArtistCredits() const;
   const std::string GetArtistString() const;
   const std::string& GetAlbum() const;
   int GetAlbumId() const;
