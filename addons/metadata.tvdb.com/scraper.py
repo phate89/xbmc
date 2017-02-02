d74ead5be593be57670f841e26db7275ae2efc24
phate89 <phates89@gmail.com>
2017-02-02 17:33:49 +0100

    Update thetvdb tvshows scraper to use our new python api

---
+++ addons/metadata.tvdb.com/scraper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from resources.lib import main
+#import time
+#start = time.time()
+
+main.run()
+
+#end = time.time()
+#with open('C:\\temp\\timing\\a{id}.txt'.format(id=sys.argv[2].lstrip('?')), 'a') as the_file:
+#    the_file.write(str(end - start))
